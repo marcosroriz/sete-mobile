3901fa6793128d60f7646710a4ea149d6d9be975
joseumbertomoreira <joseumbertomoreira@gmail.com>
2019-11-26 10:48:51 -0300

    Mudanças em ListarAlunos.js

--- ListarAlunos.js
+++ ListarAlunos.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { ActivityIndicator, Image, ScrollView, View } from 'react-native';
-import { Appbar, List, Text, Divider, Provider as PaperProvider } from 'react-native-paper';
+import { Appbar, DataTable, List, Text, Divider, Provider as PaperProvider } from 'react-native-paper';
 import * as firebase from "firebase";
 import styles from './style.js';
 
 export default class ListarAlunos extends React.Component {
+
+  Alunos = [
+      {
+        Nome: "Zezinho",
+        Escola: "Colégio Dinâmico",
+        Turno: "Integral"
+      },
+      { 
+        Nome: "Rafaelinha",
+        Escola: "Colégio do Sesi de Campinas",
+        Turno: "Verpertino"
+      }, 
+      { 
+        Nome: "Pedrinho",
+        Escola: "Escola Pedro Gomes",
+        Turno: "Matutino"
+      },
+      { 
+        Nome: "Mariazinha",
+        Escola: "Escola Assis Chateaubriand",
+        Turno: "Verpertino"
+      },
+      {
+        Nome: "Laurinha",
+        Escola: "CEPAE UFG",
+        Turno: "Verpertino"
+      }
+    ] 
 
   componentWillMount() {
     firebase.auth().onAuthStateChanged(async user => {
@@ -19,7 +47,7 @@ export default class ListarAlunos extends React.Component {
   goBackToDashboard = () => {
     this.props.navigation.navigate("Dashboard");
   }
-
+  
   render() {
     return (
       <PaperProvider>
@@ -32,10 +60,30 @@ export default class ListarAlunos extends React.Component {
           />
         </Appbar.Header>
         <View style={styles.container}>
-          <Text>
-            Listar os alunos (semelhante ao datatables)
-            permitir filtro/procurar
-          </Text>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Nome</DataTable.Title>
+            <DataTable.Title numeric>Escola</DataTable.Title>
+            <DataTable.Title numeric>Turno</DataTable.Title>
+          </DataTable.Header>
+          {
+           this.Alunos.map((alunos, index)=> {
+             return (
+              <DataTable.Row key={index}>
+                <DataTable.Cell>{alunos.Nome}</DataTable.Cell>
+                <DataTable.Cell>{alunos.Escola}</DataTable.Cell>
+                <DataTable.Cell>{alunos.Turno}</DataTable.Cell>
+              </DataTable.Row>
+             )
+           }) 
+          }
+          <DataTable.Pagination
+            page={1}
+            numberOfPages={3}
+            onPageChange={(page) => { console.log(page); }}
+            label="1-2 of 6"
+          />
+        </DataTable>
         </View>
       </PaperProvider>
     )
